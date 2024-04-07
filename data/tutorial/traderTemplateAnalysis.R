c8c5d75021891c54541fd7524f005d81b8e9b4a6
Gael Berlanga Boemare <123067687+Gael-BB@users.noreply.github.com>
2024-04-07 23:00:36 +0100

    Moving average visualizer

--- data/tutorial/traderTemplateAnalysis.R
+++ data/tutorial/traderTemplateAnalysis.R
@@ -18,12 +18,27 @@ plot(amethyst$ask_price_1)
 plot(amethyst$bid_price_1)
 
 #plot bid and ask for starfruit on same graph
-plot(starfruit$ask_price_1, type = "l", col = "red")
-lines(starfruit$bid_price_1, col = "blue")
+plot(starfruit$ask_price_1[200:350], type = "l", col = "red")
+lines(starfruit$bid_price_1[200:350], col = "blue")
 
-# 5 point moving average of starfruit mid price
-moving_average_length = 10
-moving_average = filter(starfruit$mid_price, rep(1/moving_average_length, moving_average_length))
+simple_moving_average <- function(data, window_size) {
+  n <- length(data)
+  # Calculate start and end indices for the loop to avoid NA values
+  start_index <- ceiling(window_size / 2)
+  end_index <- n - floor(window_size / 2)
+  
+  # Initialize the result vector
+  averages <- numeric(end_index - start_index + 1)
+  
+  for (i in start_index:end_index) {
+    averages[i - start_index + 1] <- mean(data[(i - floor(window_size / 2)):(i + floor(window_size / 2))])
+  }
+  
+  return(averages)
+}
 
-lines(moving_average, col = "green")
+window_size = 5  # Window size for moving average
+moving_average = simple_moving_average(starfruit$mid_price, window_size)
+
+lines(moving_average[(200-window_size):(350-window_size)], col = "green")
 
