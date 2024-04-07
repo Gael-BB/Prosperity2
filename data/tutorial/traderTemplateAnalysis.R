ffb70ea267f5c944e37a7e4357a18e5fdd27e1a1
Gael Berlanga Boemare <123067687+Gael-BB@users.noreply.github.com>
2024-04-07 22:40:13 +0100

    algorithm for starfruit implemented

--- data/tutorial/traderTemplateAnalysis.R
+++ data/tutorial/traderTemplateAnalysis.R
@@ -5,7 +5,25 @@ starfruit = data[data$product == "STARFRUIT",]
 #plot(amethyst[amethyst$day == -2,]$bid_price_1)
 #plot(starfruit[starfruit$day == -2,]$bid_price_1)
 
-plot(amethyst$mid_price[100:140])
+plot(amethyst$mid_price)
 plot(starfruit$mid_price)
+
+plot(starfruit$ask_price_1)
+plot(starfruit$bid_price_1)
+
 mean(amethyst$mid_price)
 mean(starfruit$mid_price)
+
+plot(amethyst$ask_price_1)
+plot(amethyst$bid_price_1)
+
+#plot bid and ask for starfruit on same graph
+plot(starfruit$ask_price_1, type = "l", col = "red")
+lines(starfruit$bid_price_1, col = "blue")
+
+# 5 point moving average of starfruit mid price
+moving_average_length = 10
+moving_average = filter(starfruit$mid_price, rep(1/moving_average_length, moving_average_length))
+
+lines(moving_average, col = "green")
+
--- trader.py
+++ trader.py
@@ -1,14 +1,18 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
+import numpy as np
 import string
 
 class Trader:
-    
+    past_starfruit_length = 5
+    past_starfruit = np.array([-1] * past_starfruit_length)
+
     def run(self, state: TradingState):
         result = {}
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
+            #position = state.position[product]
 
             match(product):
                 case "AMETHYSTS":
@@ -27,11 +31,37 @@ class Trader:
                         if int(best_bid) > acceptable_price:
                             print("SELL", str(best_bid_amount) + "x", best_bid)
                             orders.append(Order(product, best_bid, -best_bid_amount))
+                
+                case "STARFRUIT":
+                    if len(order_depth.sell_orders) != 0:
+                        best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                    else:
+                        best_ask, best_ask_amount = self.past_starfruit[0], 0
+                    
+                    if len(order_depth.buy_orders) != 0:
+                        best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                    else:
+                        best_bid, best_bid_amount = self.past_starfruit[0], 0
+
+                    self.past_starfruit = np.roll(self.past_starfruit, 1)
+                    self.past_starfruit[0] = np.mean([best_ask, best_bid])
+                    acceptable_price = np.mean(self.past_starfruit)
+
+                    if self.past_starfruit[self.past_starfruit_length-1] != -1:
+
+                        if len(order_depth.sell_orders) != 0:
+                            if int(best_ask) < acceptable_price:
+                                print("BUY", str(-best_ask_amount) + "x", best_ask)
+                                orders.append(Order(product, best_ask, -best_ask_amount))
+            
+                        if len(order_depth.buy_orders) != 0:
+                            if int(best_bid) > acceptable_price:
+                                print("SELL", str(best_bid_amount) + "x", best_bid)
+                                orders.append(Order(product, best_bid, -best_bid_amount))
 
             result[product] = orders
     
     
         traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
-        
         conversions = 1
         return result, conversions, traderData
