c384220c5e9cd49dfd759e27a9679f7fc109b2b3
Gael Berlanga Boemare <123067687+Gael-BB@users.noreply.github.com>
2024-04-10 21:14:33 +0100

    Trader Update

--- data/round1/regression.R
+++ data/round1/regression.R
@@ -2,18 +2,32 @@ day_n2 = read.csv("prices_round_1_day_-2.csv", header = TRUE, sep = ";")
 day_n1 = read.csv("prices_round_1_day_-1.csv", header = TRUE, sep = ";")
 day_0 = read.csv("prices_round_1_day_0.csv", header = TRUE, sep = ";")
 
-X = rbind(day_n2[day_n2$product == "STARFRUIT",], day_n1[day_n1$product == "STARFRUIT",], day_0[day_0$product == "STARFRUIT",])
-#X = day_n2[day_n2$product == "STARFRUIT",]
-#columns = c("bid_price_1", "bid_price_2", "bid_price_3", "bid_volume_1", "bid_volume_2", "bid_volume_3", "ask_price_1", "ask_price_2", "ask_price_3", "ask_volume_1", "ask_volume_2", "ask_volume_3")
+df = rbind(day_n2, day_n1, day_0)
+df = df[df$product == 'STARFRUIT',]
 
+# Target
+mid_price_change = df$mid_price / c(NA, head(df$mid_price, -1)) - 1
 
-X$total_ask_volume = rowSums(X[, c("ask_volume_1", "ask_volume_2", "ask_volume_3")], na.rm = TRUE)
-X$total_bid_volume = rowSums(X[, c("bid_volume_1", "bid_volume_2", "bid_volume_3")], na.rm = TRUE)
+# Price based features
+ask_price_change = df$ask_price_1 / c(NA, head(df$ask_price_1, -1)) - 1
+bid_price_change = df$bid_price_1 / c(NA, head(df$bid_price_1, -1)) - 1
+bid_ask_spread =  df$ask_price_1 - df$bid_price_1
 
-columns = c("bid_price_1", "bid_volume_1", "ask_price_1", "ask_volume_1", "total_ask_volume", "total_bid_volume")
-Y = diff(X$mid_price)
-X = X[-1,columns]
+# Volume based features
+total_bid_volume = rowSums(df[,c("bid_volume_1", "bid_volume_2", "bid_volume_3")], na.rm = TRUE)
+total_ask_volume = rowSums(df[,c("ask_volume_1", "ask_volume_2", "ask_volume_3")], na.rm = TRUE)
+volume_imbalance = total_ask_volume - total_bid_volume
+
+X = data.frame(ask_price_change, bid_price_change, bid_ask_spread, volume_imbalance)[-1:-2,]
+Y = c(NA, head(mid_price_change, -1))[-1:-2]
 
 model = lm(Y ~ ., data = X)
 summary(model)
 
+library(glmnet)
+
+set.seed(123) # For reproducibility
+cv.fit <- cv.glmnet(as.matrix(X), Y, alpha = 1)
+
+# Look at the coefficients
+coef(cv.fit, s = "lambda.min") # Coefficients at the lambda that gives minimum mean cross-validated error
--- trader.py
+++ trader.py
@@ -1,98 +1,116 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import numpy as np
-import string
+import jsonpickle
 
 # back tester command: & 'c:\Users\Gael Work\AppData\Roaming\Python\Python312\Scripts\prosperity2bt.exe' trader.py 1
 class Trader:
-    # Starfuit exponential moving average variables
-    starfruit_period = 4
-    starfruit_smoothing_factor = 1/(starfruit_period+1)
-    starfruit_ema = -1
-    starfruit_past_best_ask = -1
-    starfruit_past_best_bid = -1
-    
-    max_positions = {"AMETHYSTS": 20, "STARFRUIT": 20}
+    def calculate_weighted_mid_price(self, order_depth):
+        if len(order_depth.sell_orders) == 0 or len(order_depth.buy_orders) == 0:
+            return None
+        
+        total_price = 0
+        total_volume = 0
+        for ask_price, ask_volume in list(order_depth.sell_orders.items()):
+            total_price -= ask_price * ask_volume
+            total_volume -= ask_volume
+        
+        for bid_price, bid_volume in list(order_depth.buy_orders.items()):
+            total_price += bid_price * bid_volume
+            total_volume += bid_volume
+        
+        return total_price / total_volume
 
-    def acceptable_price_trade(self, product, order_depth, position, max_position, acceptable_price) -> List[Order]:
-        if (abs(position) == max_position): print(f"WARNING: {product} is at position {position}.")
-        orders = []
-        virtual_position = position
-        for ask, ask_amount in list(order_depth.sell_orders.items()):
-            if ask < acceptable_price and position != max_position:
-                amount = np.minimum(max_position - position, -ask_amount).item()
-                print(f"Buying {product} at {ask} with {amount}x.")
-                orders.append(Order(product, ask, amount))
-                virtual_position += amount
-            
-        for bid, bid_amount in list(order_depth.buy_orders.items()):
-            if bid > acceptable_price and position != -max_position:
-                amount = np.minimum(max_position + position, bid_amount).item()
-                print(f"Selling {product} at {bid} with {amount}x.")
-                orders.append(Order(product, bid, -amount))
-                virtual_position -= amount
-        return orders
+
+    def starfruit_calculate_exponential_ma(self, data, order_depth):
+        mid_price = self.calculate_weighted_mid_price(order_depth)
+        
+        if mid_price == None:
+            print("WARNING: No mid price available.")
+            return data, False
+        
+        if data['ema'] == -1:
+            data['ema'] = mid_price
+            print("Initialized exponential moving average.")
+            return data, False
+        
+        data['ema'] = data['smoothing_factor'] * mid_price + (1 - data['smoothing_factor']) * data['ema']
+        return data, True
+    
+    def starfruit_calculatet_linear_regression(self, data, order_depth):
+        total_ask_volume = 0
+        total_bid_volume = 0
+
+        for ask in list(order_depth.sell_orders.items()): total_ask_volume += ask[1]
+        for bid in list(order_depth.buy_orders.items()): total_bid_volume += bid[1]
+        
+        if len(order_depth.sell_orders) != 0: best_ask = list(order_depth.sell_orders.items())[0][0]
+        else: best_ask = data['past_best_ask']
+
+        if len(order_depth.buy_orders) != 0: best_bid = list(order_depth.buy_orders.items())[0][0]
+        else: best_bid = data['past_best_bid']
+
+        pct_change = -4.700e-5 + (best_ask / data['past_best_ask'] - 1) * -1.878e-01 + total_ask_volume * 2.207e-05 + (best_bid / data['past_best_bid'] - 1) * -2.022e-01 + total_bid_volume * -2.028e-05
+        data['regression'] = (1 + pct_change) * (best_ask + best_bid) / 2
+
+        data['past_best_ask'] = best_ask
+        data['past_best_bid'] = best_bid
+        
+        if data['past_best_ask']== -1 or data['past_best_bid'] == -1:
+            print("WARNING: Missing best ask or bid values!")
+            return data, False
+        
+        return data, True
+        
 
     def run(self, state: TradingState):
         result = {}
+        if state.timestamp == 0:
+            # ALL VARIABLES THAT NEED STORAGE GO HERE
+            traderData = {}
+
+            # Amethysts Data
+            amethysts_data = {'max_position': 20}
+            traderData['AMETHYSTS'] = amethysts_data
+
+            # Starfuit Data
+            starfruit_data = {'max_position': 20, 'past_best_ask': -1, 'past_best_bid': -1, 'ema-period': 4, 'ema': -1, 'regression': -1}
+            starfruit_data['smoothing_factor'] = 1 / (starfruit_data['ema-period'] + 1)
+            traderData['STARFRUIT'] = starfruit_data
+        else:
+            traderData = jsonpickle.decode(state.traderData)
+
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            
-            # Find position and max_position
-            max_position = self.max_positions[product]
-            try:
-                position = state.position[product]
-            except:
-                position = 0
-                print(f"Position not found for {product}. Setting to 0")
 
+            data = traderData[product]
+            position = state.position.get(product, 0)
+            max_position = data['max_position']
             match(product):
                 case "AMETHYSTS":
                     acceptable_price = 10000
-                    # orders.append(Order(product, acceptable_price-2, max_position - position))
-                    # orders.append(Order(product, acceptable_price+2, -max_position - position))
 
-                    pricing_increment_positions = [0, 15, 18, 20]
-                    
-                    if abs(position) == 20: print("WARNING: Amethyst is at max position.")
-                    print(f"Amethyst is at position {position}.")
-                    for i in range(1, len(pricing_increment_positions)):
-                        orders.append(Order(product, acceptable_price - (i+1), np.maximum(0,np.minimum(pricing_increment_positions[i]-pricing_increment_positions[i-1],pricing_increment_positions[i]-position)).item()))
-                        orders.append(Order(product, acceptable_price + (i+1), np.minimum(0,np.maximum(pricing_increment_positions[i-1]-pricing_increment_positions[i],-pricing_increment_positions[i]-position)).item()))
+                    orders.append(Order(product, acceptable_price-2, max_position - position))
+                    orders.append(Order(product, acceptable_price+2, -max_position - position))
                 
                 case "STARFRUIT":
-                    break
-                    # Calculate Exponential Moving Average
-                    if len(order_depth.sell_orders) != 0:
-                        self.starfruit_past_best_ask = list(order_depth.sell_orders.items())[0][0]
-                    if len(order_depth.buy_orders) != 0:
-                        self.starfruit_past_best_bid = list(order_depth.buy_orders.items())[0][0]
+                    data, tradable = self.starfruit_calculate_exponential_ma(data, order_depth)
+                    acceptable_price = data['ema']
 
-                    if self.starfruit_past_best_ask == -1 or self.starfruit_past_best_bid == -1:
-                        print("No best bid or ask for Starfruit")
-                        break
+                    # data, tradable = self.starfruit_calculatet_linear_regression(data, order_depth)
+                    # acceptable_price = data['regression']
 
-                    mid_price = np.mean([self.starfruit_past_best_ask, self.starfruit_past_best_bid])
-                    if self.starfruit_ema == -1:
-                        self.starfruit_ema = mid_price
-                    
-                    self.starfruit_ema = self.starfruit_smoothing_factor * mid_price + (1 - self.starfruit_smoothing_factor) * self.starfruit_ema
-                    acceptable_price = self.starfruit_ema
-
-
-                    """ask_price, ask_volume = list(order_depth.sell_orders.items())[0]
-                    bid_price, bid_volume = list(order_depth.buy_orders.items())[0]
-                    acceptable_price = (-3.4868403 + 0.5 * (ask_price + bid_price)) + (0.0465762 * bid_price) + (-0.0438979 * bid_volume) + (-0.0458501 * ask_price) + (0.0471026 * ask_volume)"""
-
-                    # Trade Starfruit according to it's acceptable price, product has max position of 20
-                    current_orders = self.acceptable_price_trade(product, order_depth, position, 20, acceptable_price)
-                    if len(current_orders) != 0:
-                        orders += current_orders
-
+                    if tradable:
+                        orders.append(Order(product, int(round(acceptable_price))-2, max_position - position))
+                        orders.append(Order(product, int(round(acceptable_price))+2, -max_position - position))
+            
+            
+            
+            # Don't modify anything below this comment
+            traderData[product] = data
             result[product] = orders
-    
-    
-        traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
+        # Notice the indentation    
+        traderData = jsonpickle.encode(traderData)
         conversions = 1
         return result, conversions, traderData
